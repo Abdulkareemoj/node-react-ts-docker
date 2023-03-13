14fa38aed9244a4030d6d9b2716a7421dd5a31b7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-13 20:29:07 +0000

    restart

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("help")
+import express from 'express';
+import config from 'config';
+
+const app = express();
+const port = config.get('port');
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http//localhost:{port}`);
+    });
