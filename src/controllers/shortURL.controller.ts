66496adc5c8b6031a03ea9a9490737dced955864
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-14 19:49:44 +0000

    added controller logic

---
+++ src/controllers/shortURL.controller.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from 'express'
+import shortURL from '../models/shortURL.model';
+
+export async function createShortURL(req: Request, res: Response)){
+
+    const { destination } = req.body;
+const shortURL = await shortURL.create({destination});
+return res.send
+}
