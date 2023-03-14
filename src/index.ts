21a492f931fd3739b9f545c49cb580a8b37dc91b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-14 19:48:19 +0000

    fixed port impor and added routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import config from 'config';
+import express from 'express'
+import config from 'config'
+import routes from './routes'
 
 const app = express();
 const port = config.get('port');
 
 
 app.listen(port, () => {
-    console.log(`Server is running at http//localhost:{port}`);
-    });
+    console.log(`Server is running at http://localhost:${port}`);
+   routes(app)
+});
