8d2e7d0fc29dc1af686bef949f78247e2e3e4622
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-15 22:23:27 +0000

    fixed port call

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express'
-import config from 'config'
-import routes from './routes'
+import express from "express";
+import config from "config";
+import routes from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
-const port = config.get('port');
+const port = config.get("port");
 
+app.use(bodyParser.json);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-   routes(app)
+  console.log(`Server is running at http://localhost:${port}`);
+  routes(app);
 });
