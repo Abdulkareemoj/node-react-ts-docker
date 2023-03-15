1b3413044b15c806649e9484a039fcf64e912ec1
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-15 22:22:14 +0000

    fixed funcion call

--- src/controllers/shortURL.controller.ts
+++ src/controllers/shortURL.controller.ts
@@ -1,9 +1,8 @@
-import {Request, Response} from 'express'
-import shortURL from '../models/shortURL.model';
+import { Request, Response } from "express";
+import shortURL from "../models/shortURL.model";
 
-export async function createShortURL(req: Request, res: Response)){
-
-    const { destination } = req.body;
-const shortURL = await shortURL.create({destination});
-return res.send
+export async function createShortURL(req: Request, res: Response) {
+  const { destination } = req.body;
+  const newURL = await shortURL.create({ destination });
+  return res.send(newURL);
 }
