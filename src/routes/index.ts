e531958b51336d63cf181e0a233e7554ad33ade5
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-18 14:08:05 +0000

    validate route and added redirection route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Express, Request, Response } from "express";
-import { createShortURL } from "../controllers/shortURL.controller";
+import { createShortURL, redirectURL } from "../controllers/shortURL.controller.js";
+import validateResource from "../middleware/validator.js";
+import createShortURLSchema from "../schema/createShortURL.schema.js";
 
 function routes(app: Express) {
   app.get("/", (req: Request, res: Response) => {
     return res.send("bruh");
   });
 
-  app.post("/api/createurl", createShortURL);
+  app.post("/api/createurl", validateResource(createShortURLSchema), createShortURL);
+
+  app.get("/:shortId", redirectURL);
 }
 
 export default routes;
