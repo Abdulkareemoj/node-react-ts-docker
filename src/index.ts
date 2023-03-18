6817306712a16ccea955af4f03a0c6c339408ea9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-18 14:05:48 +0000

    added database function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-// import config from "config";
-import routes from "./routes"
+import config from "config";
+import routes from "./routes/index.js"
 import bodyParser from "body-parser";
 import express from "express";
-
+import db from "./utils/db.js"
 
 const app = express();
-const port = 3000;
+const port = config.get("port")
 
 app.use(bodyParser.json);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-  routes(app);
+ db(); 
+ routes(app);
 });
