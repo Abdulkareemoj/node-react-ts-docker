fee92fe32e224ff520bfd98efea521b859e8f1cf
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-18 14:06:44 +0000

    link creation controller

--- src/controllers/shortURL.controller.ts
+++ src/controllers/shortURL.controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
-import shortURL from "../models/shortURL.model";
+import shortURL from "../models/shortURL.model.js";
 
 export async function createShortURL(req: Request, res: Response) {
   const { destination } = req.body;
   const newURL = await shortURL.create({ destination });
   return res.send(newURL);
+}
+
+export async function redirectURL (req: Request, res: Response) {
+  const {shortId} =req.params;
+  const short = await shortURL.findOne({ shortId}).lean();
+  if (!short){
+    return res.sendStatus(404); 
+  }
+
+
+  return res.redirect(short.destination);
 }
