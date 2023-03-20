acacc83a884290e904aebee9dbf1e18b04b72117
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-20 20:55:35 +0000

    update controller

--- src/controllers/shortURL.controller.ts
+++ src/controllers/shortURL.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import shortURL from "../models/shortURL.model.js";
+import analytics from "../models/analytics.model.js"
 
 export async function createShortURL(req: Request, res: Response) {
   const { destination } = req.body;
@@ -13,7 +14,12 @@ export async function redirectURL (req: Request, res: Response) {
   if (!short){
     return res.sendStatus(404); 
   }
-
+analytics.create(shortURL: short._id)
 
   return res.redirect(short.destination);
+}
+
+export async function analytics (req: Request, res: Response) {
+const data = analytics.find({}).lean()
+return res.send(data); 
 }
