b70c53027f824fec4982b5185250d8eec5c7d5d0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-24 22:43:25 +0100

    add cors for frontend

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import routes from "./routes/index.js"
 import bodyParser from "body-parser";
 import express from "express";
 import db from "./utils/db.js"
+import cors from 'cors'
 
 const app = express();
+app.use(cors({
+  origin: config.get("corsOrigin")}
+))
+
 const port = config.get("port")
 
 app.use(bodyParser.json);
