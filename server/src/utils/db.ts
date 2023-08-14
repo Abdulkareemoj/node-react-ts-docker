7b77e2c693e8fade4c8afb89b75f0997f929bc3f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-08-14 21:45:29 +0100

    db connection

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
-import config from "config"
+import config from "config";
 
-async function db(){
-  const dbUri  = config.get("dbUrl") as string;
-  try{
-    await mongoose
-    .connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-  console.log(`Connected to DB at ${dbUri}`);
+async function db() {
+  const dbUri = config.get("dbUrl") as string;
+  try {
+    await mongoose.connect(`${dbUri}`);
 
-})
-  }  catch (e){
+    console.log(`Connected to DB at ${dbUri}`);
+  } catch (e) {
     console.error(e);
   }
 }
