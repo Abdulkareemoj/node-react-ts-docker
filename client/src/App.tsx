cc8496831c081cb835930ce76bea237d951ce274
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-01 10:43:03 +0100

    add links page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HandleRedirect from './components/HandleRedirect';
-import Home from './components/Home';
+import Home from './pages/Home';
+import Links from './pages/Links';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:shortId" element={<HandleRedirect />} />
-      </Routes>
-    </Router>
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/links" element={<Links />} />
+          <Route path="/:shortId" element={<HandleRedirect />} />
+        </Routes>
+      </Router>
+    </Layout>
   );
 }
 
