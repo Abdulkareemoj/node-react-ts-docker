cc86df8d762e169e7102ca7b01fc690e6ca7d524
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-02 09:21:27 +0100

    fix envs

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
+dotenv.config();
 async function db() {
   const dbUri = process.env.MONGO_URI as string;
   try {
