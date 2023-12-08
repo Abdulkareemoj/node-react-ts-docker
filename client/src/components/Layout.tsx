a40ade54b7ebd7c8ff62e4d7d0aa3a942e561269
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-08 11:47:00 +0100

    changed to a sidebar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,14 +1,17 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 // Pass the child props
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Box>
-      <Navbar />
-      <Box mx="20">{children}</Box>
-
+    <>
+      <Flex>
+        <Navbar />
+        <Box flex="1" mx="20">
+          {children}
+        </Box>
+      </Flex>
       <Footer />
-    </Box>
+    </>
   );
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,37 +1,36 @@
-import { useState } from 'react';
-import { useColorMode, Switch, Flex, Button, Spacer } from '@chakra-ui/react';
+import { useColorMode, Switch, Flex, Spacer } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Link as ChakraLink, LinkProps } from '@chakra-ui/react';
+import { Link as ChakraLink } from '@chakra-ui/react';
 
-// export default function Navbar() {
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === 'dark';
 
   return (
-    <>
-      <Flex px="10">
-        {/* Desktop */}
-        <Flex>
-          <ChakraLink as={ReactRouterLink} to="/" p="5">
-            Home
-          </ChakraLink>
-          <ChakraLink as={ReactRouterLink} to="/links" p="5">
-            Links
-          </ChakraLink>
-          <ChakraLink as={ReactRouterLink} to="/contact" p="5">
-            Contact
-          </ChakraLink>
-        </Flex>
-        <Spacer />
-        <Switch
-          color="green"
-          isChecked={isDark}
-          onChange={toggleColorMode}
-          p="5"
-        />
-      </Flex>
-    </>
+    <Flex
+      as="nav"
+      flexDirection="column"
+      p="10"
+      w="200px"
+      h="100vh"
+      borderRightWidth="1px"
+    >
+      <ChakraLink as={ReactRouterLink} to="/" p="5">
+        Home
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/links" p="5">
+        Links
+      </ChakraLink>
+      <ChakraLink as={ReactRouterLink} to="/contact" p="5">
+        Contact
+      </ChakraLink>
+      <Spacer />
+      <Switch
+        color="green"
+        isChecked={isDark}
+        onChange={toggleColorMode}
+        p="5"
+      />
+    </Flex>
   );
 }
-// }
