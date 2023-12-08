fe16532a67fe1b613c4963dcaf4010fb2edb86aa
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-08 13:29:03 +0100

    rework home view

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,23 @@
 import { Box } from '@chakra-ui/react';
 
 import URLShortenerForm from '../components/URLShortener';
+import Header from '../components/Header';
 
 function Home() {
   return (
-    <Box
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <URLShortenerForm />
-    </Box>
+    <>
+      <Box>
+        <Header />
+      </Box>
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <URLShortenerForm />
+      </Box>
+    </>
   );
 }
 export default Home;
