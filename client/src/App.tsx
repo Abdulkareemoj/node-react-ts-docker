14ffebb849789d231ebddc91de9298f88103612f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-09 18:03:34 +0100

    separated pages that need layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HandleRedirect from './components/HandleRedirect';
 import Home from './pages/Home';
 import Links from './pages/Links';
+import Landing from './pages/Landing';
 import Layout from './components/Layout';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/links" element={<Links />} />
-          <Route path="/:shortId" element={<HandleRedirect />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/links" element={<Links />} />
+                <Route path="/:shortId" element={<HandleRedirect />} />
+              </Routes>
+            </Layout>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
