c18536bd6ffe3f0482774d3824833efaf3d37c2a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-14 22:04:41 +0100

    more tinkering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,9 @@ import HandleRedirect from './components/HandleRedirect';
 import Home from './pages/Home';
 import Links from './pages/Links';
 import Landing from './pages/Landing';
-import Layout from './components/Layout';
+import DashboardLayout from './components/DashboardLayout';
+import LandingLayout from './components/LandingLayout';
+
 import Contact from './pages/Contact';
 import SignIn from './pages/SignIn';
 import About from './pages/About';
@@ -12,22 +14,17 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Routes>
-                <Route index element={<Home />} />
-                <Route path="/links" element={<Links />} />
-                <Route path="/:shortId" element={<HandleRedirect />} />
-              </Routes>
-            </Layout>
-          }
-        />
+        <Route path="/*" element={<LandingLayout />}>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+        <Route path="/*" element={<DashboardLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/links" element={<Links />} />
+          <Route path="/:shortId" element={<HandleRedirect />} />
+        </Route>
       </Routes>
     </Router>
   );
