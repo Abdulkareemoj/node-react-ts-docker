c8df75b3506d3b632c2a58aee0649b82a1749ff9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-29 14:30:45 +0100

    moved pages

--- client/src/pages/dashboard/Home.tsx
+++ client/src/pages/dashboard/Home.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react';
-import Table from '../components/Table';
-import URLShortenerForm from '../components/URLShortener';
-import Header from '../components/Header';
+import Table from '../../components/Table';
+import URLShortenerForm from '../../components/URLShortener';
+import Header from '../../components/Header';
 
 function Home() {
   return (
