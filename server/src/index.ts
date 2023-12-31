61d69a536ad52239a02d771fd20a321882f53e42
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-12-31 22:15:36 +0100

    fix env isse

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,15 @@ import bodyParser from "body-parser";
 import express from "express";
 import db from "./utils/db";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envPath = path.resolve(__dirname, "../../.env");
+dotenv.config({ path: envPath });
 
 const app = express();
 app.use(cors({
@@ -15,7 +21,7 @@ app.use(cors({
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
   db();
