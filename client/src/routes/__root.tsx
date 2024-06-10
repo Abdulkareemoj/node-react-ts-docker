6502e0df428ad961a61a8bed3a4c2c5fedd08290
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-06-10 12:54:02 +0100

    help 3

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+      <ReactQueryDevtools buttonPosition="top-right" />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
