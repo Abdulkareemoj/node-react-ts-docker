e7352ae643590f3b7d912959c84bbc3b813ebbe7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-06-10 12:52:29 +0100

    help 2

--- client/src/routes/About.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/About")({
-  component: About,
-});
-
-function About() {
-  return <div>About</div>;
-}
