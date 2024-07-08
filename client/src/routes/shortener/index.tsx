93c7203ec4d1edd2e4a8ec68d5ef01907d16b8b3
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-08 10:41:33 +0100

    feat: Add URLShortener component and route for /shortener/

---
+++ client/src/routes/shortener/index.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/shortener/")({
+  component: URLShortener,
+});
+function URLShortener() {
+  return <main>
+    
+  </main>;
+}
