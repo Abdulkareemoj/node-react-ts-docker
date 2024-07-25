b5c0e63b079b050992b2508dd2c88c5ace379dac
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-25 13:12:45 +0100

    chore: Refactor createUserHandler to extract roles from request body

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,18 +1,36 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { omit } from "lodash-es";
 import { createUser } from "../service/user.service";
 import log from "../logger";
 import { CreateUserInput } from "../schema/user.schema";
+
+// Example RBAC middleware
+// function authorize(roles: string[]) {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     const userRoles = res.locals.user.roles;
+//     const hasPermission = roles.some((role) => userRoles.includes(role));
+//     if (!hasPermission) {
+//       return res
+//         .status(403)
+//         .send("You do not have permission to perform this action");
+//     }
+//     next();
+//   };
+// }
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput["body"]>,
   res: Response
 ) {
   try {
-    const user = await createUser(req.body);
-    //omit password
+    // Destructure the roles field from the request body
+    const { roles, ...restOfBody } = req.body;
+
+    // Pass the extracted roles along with the rest of the user data to createUser
+    const user = await createUser({ ...restOfBody, roles });
+
+    // Omit password from the response
     return res.send(omit(user.toJSON(), "password"));
-    //look at previous commit incase this doesnt work out well edd74a1
   } catch (e: unknown) {
     log.error(e);
     if (e instanceof Error) {
