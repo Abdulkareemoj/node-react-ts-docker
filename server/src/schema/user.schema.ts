124a7ce69a902aa7c3e8bfefa5a697b0c99159e0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-25 13:19:53 +0100

    chore: Update createUser function to include roles in UserInput

--- server/src/schema/user.schema.ts
+++ server/src/schema/user.schema.ts
@@ -1,4 +1,4 @@
-import { TypeOf, object, string } from "zod";
+import { TypeOf, array, object, string } from "zod";
 
 export const createUserSchema = object({
   body: object({
@@ -12,6 +12,24 @@ export const createUserSchema = object({
       required_error: "confirmation is required",
     }),
     // .oneOf([ref("password"), null], "Password must match"),
+    roles: array(
+      string({
+        required_error: "At least one role is required",
+      })
+    )
+      .min(1, "At least one role is required")
+      .refine(
+        (data) =>
+          data.every((role) =>
+            [
+              "user", //"editor", "viewer"
+            ].includes(role)
+          ),
+        {
+          message: "Invalid role",
+        }
+      )
+      .default(["user"]),
     email: string({ required_error: "Email is required" }).email(
       "Must be a valid email"
     ),
@@ -38,3 +56,56 @@ export type CreateUserInput = Omit<
   TypeOf<typeof createUserSchema>,
   "body.passwordConfirmation"
 >;
+
+// Admin User Schema
+export const createAdminSchema = object({
+  body: object({
+    name: string({
+      required_error: "Name is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password is too short"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+    email: string({ required_error: "Email is required" }).email(
+      "Must be a valid email"
+    ),
+    roles: array(
+      string({
+        required_error: "At least one role is required",
+      })
+    )
+      .min(1, "At least one role is required")
+      .refine(
+        (data) => data.every((role) => ["admin", "superadmin"].includes(role)),
+        {
+          message: "Invalid role",
+        }
+      )
+      .default(["admin"]),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  }),
+});
+
+// Admin Session Schema
+export const createAdminSessionSchema = object({
+  body: object({
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password is too short"),
+    email: string({ required_error: "Email is required" }).email(
+      "Must be a valid email"
+    ),
+  }),
+});
+
+export type CreateAdminInput = Omit<
+  TypeOf<typeof createAdminSchema>,
+  "body.passwordConfirmation"
+>;
+
+export type CreateAdminSessionInput = TypeOf<typeof createAdminSessionSchema>;
