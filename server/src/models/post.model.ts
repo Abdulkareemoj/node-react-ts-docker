9170f09e5f3e167d3f432d832430a1f118dfa2a9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-05 11:15:00 +0100

    chore: Update Post model to include additional fields

--- server/src/models/post.model.ts
+++ server/src/models/post.model.ts
@@ -1,22 +1,35 @@
-import mongoose from 'mongoose';
-import {  UserDocument } from './user.model'
+import mongoose from "mongoose";
+import { UserDocument } from "./user.model";
 
-export interface PostDocument extends mongoose.Document{
-    user: UserDocument["_id"];
-    valid: boolean;
-    userAgent: string;
-    timestamps: Date;
+export interface PostDocument extends mongoose.Document {
+  user: UserDocument["_id"];
+  title: string;
+  desc: string;
+  img: string;
+  authorLogo: string;
+  authorName: string;
+  href: string;
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const PostSchema = new mongoose.Schema(
-    {
-        user: {type: mongoose.Schema.Types.ObjectId, ref: "user"},
-        valid: {type: Boolean, default: true},
-userAgent:{type: String},
-    },
-    {timestamps: true}
-)
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    authorLogo: { type: String, required: true },
+    authorName: { type: String, required: true },
+    href: { type: String, required: true },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String },
+  },
+  { timestamps: true }
+);
 
-const Post = mongoose.model<PostDocument>("Post", PostSchema)
+const Post = mongoose.model<PostDocument>("Post", PostSchema);
 
-export default Post
+export default Post;
