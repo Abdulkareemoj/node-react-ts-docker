bac7b1e67766e1d5ac2d759491512cfef8a25a42
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-09 11:24:44 +0100

    refactor: Update Product model to include required fields and timestamps

--- server/src/models/product.model.ts
+++ server/src/models/product.model.ts
@@ -4,17 +4,14 @@ import { customAlphabet } from "nanoid";
 
 const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
 
-
-export interface ProductInput{
-    user: UserDocument["_id"];
+export interface ProductInput {
+  user: UserDocument["_id"];
   title: string;
   description: string;
   price: string;
   image: string;
   timestamps: Date;
 }
-
-
 
 const ProductSchema = new mongoose.Schema(
   {
@@ -26,16 +23,15 @@ const ProductSchema = new mongoose.Schema(
     },
 
     user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-    title: { type: String, required: true, },
+    title: { type: String, required: true },
     description: { type: String, required: true },
-    price: {type: String, required: true,  },
-    image: {type: String, required: true,  },
-    },
+    price: { type: String, required: true },
+    image: { type: String, required: true },
+  },
   {
     timestamps: true,
-  },
-
-); 
+  }
+);
 
 const Product = mongoose.model("Product", ProductSchema);
 
