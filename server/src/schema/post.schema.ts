4b5bff2c677b53bf41531db2c7cbb403c7a31bae
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-09 09:34:44 +0100

    refactor: Update Product and Post models to include required fields and timestamps

--- server/src/schema/post.schema.ts
+++ server/src/schema/post.schema.ts
@@ -1,40 +1,47 @@
-import { TypeOf, z } from "zod"
+import { TypeOf, z } from "zod";
 
 const payload = {
-    body: z.object({
-        title: z.string({required_error:"Title is required"}),
-        body: z.string({required_error:"Body is required"}).min(120, "Body is too short, 120 char min")
-    }),
-} 
+  body: z.object({
+    title: z.string({ required_error: "Title is required" }),
+    description: z
+      .string({ required_error: "Description is required" })
+      .min(20),
+    image: z
+      .string({ required_error: "Image URL is required" })
+      .url("Must be a valid URL"),
+    authorName: z.string({ required_error: "Author name is required" }),
+    href: z
+      .string({ required_error: "Href is required" })
+      .url("Must be a valid URL"),
+  }),
+};
 const params = {
-    params: z.object({
-        postId: z.string({required_error:"postId required"})
-    }),
-}
+  params: z.object({
+    postId: z.string({ required_error: "postId required" }),
+  }),
+};
 
 export const createPostSchema = z.object({
-    ...payload,
-})
+  ...payload,
+});
 
 export const updatePostSchema = z.object({
-   ...params,
-    ...payload,
-})
+  ...params,
+  ...payload,
+});
 export const getPostSchema = z.object({
-   ...params,
-    ...payload,
-})
-
+  ...params,
+  ...payload,
+});
 
 export const deletePostSchema = z.object({
-...params,
-})
+  ...params,
+});
 
+export type createPostInput = TypeOf<typeof createPostSchema>;
 
-export type createPostInput = TypeOf<typeof createPostSchema>
+export type updatePostInput = TypeOf<typeof updatePostSchema>;
 
-export type updatePostInput = TypeOf<typeof updatePostSchema>
+export type readPostInput = TypeOf<typeof getPostSchema>;
 
-export type readPostInput = TypeOf<typeof getPostSchema>
-
-export type deletePostInput = TypeOf<typeof deletePostSchema>
+export type deletePostInput = TypeOf<typeof deletePostSchema>;
