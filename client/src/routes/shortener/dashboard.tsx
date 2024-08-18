88fa7c59bd2c4f66c2f8560f1fd99641eb6742a9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-18 14:37:17 +0100

    yikes

--- client/src/routes/shortener/dashboard.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/shortener/dashboard")({
-  component: () => <div>Hello /shortener/dashboard!</div>,
-});
