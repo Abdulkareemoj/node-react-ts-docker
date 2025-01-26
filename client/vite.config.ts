4766b5bd073de67780b95ac3c37968b8e6fce470
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-01-26 10:28:08 +0100

    prepare for taailwind 4.0

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), TanStackRouterVite(), tsconfigPaths()],
 });
