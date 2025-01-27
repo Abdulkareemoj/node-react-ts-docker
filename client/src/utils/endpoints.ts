6f989771c49e382ab413c53878700dcc284e6ee8
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-01-27 17:08:16 +0100

    feat: add request interceptor to axiosClient for token authorization

--- client/src/utils/endpoints.ts
+++ client/src/utils/endpoints.ts
@@ -3,3 +3,16 @@ import axios from "axios";
 export const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_BASE_URL || "http://localhost:7000",
 });
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // Get the token from localStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Set the token in the Authorization header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
