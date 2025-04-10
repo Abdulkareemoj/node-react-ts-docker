9e8d7dba3bee3eb561a73fcc6cd91589f288b9be
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-04-10 14:57:24 +0100

    minor chnage

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 import React, { useState, ReactNode } from "react";
 import Sidebar from "../components/dashboard/Sidebar/index";
 import Header from "../components/dashboard/Header";
-import ProtectedRoute from "@/components/shared/protectedRoute";
+// import ProtectedRoute from "@/components/shared/protectedRoute";
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -10,25 +10,25 @@ interface DashboardLayoutProps {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <ProtectedRoute>
-      <div className="dark:bg-boxdark-2 dark:text-bodydark h-screen">
-        {/* Page Wrapper */}
-        <div className="flex h-full">
-          {/* Sidebar */}
-          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    // <ProtectedRoute>
+    <div className="dark:bg-boxdark-2 dark:text-bodydark h-screen">
+      {/* Page Wrapper */}
+      <div className="flex h-full">
+        {/* Sidebar */}
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-          {/* Content Area */}
-          <div className="flex-1 flex flex-col overflow-hidden">
-            {/* Header */}
-            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {/* Content Area */}
+        <div className="flex-1 flex flex-col overflow-hidden">
+          {/* Header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-            {/* Main Content */}
-            <main className="flex-1 overflow-y-auto bg-gray-100 p-6">
-              {children}
-            </main>
-          </div>
+          {/* Main Content */}
+          <main className="flex-1 overflow-y-auto bg-gray-100 p-6">
+            {children}
+          </main>
         </div>
-      </div>{" "}
-    </ProtectedRoute>
+      </div>
+    </div>
+    // </ProtectedRoute>
   );
 }
