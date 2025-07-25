18adc4b94dde918f4b3e5877bc6447bb2e8e0fc0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-07-25 18:58:30 +0100

    fixes

--- client/src/routes/admin/contacts.tsx
+++ client/src/routes/admin/contacts.tsx
@@ -14,13 +14,6 @@ function AdminContacts() {
       <div className="flex flex-1 flex-col gap-4 lg:gap-6 py-4 lg:py-6">
         {/* Page intro */}
         <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1">
-            <h1 className="text-2xl font-semibold">Oilà, Larry!</h1>
-            <p className="text-sm text-muted-foreground">
-              Here&rsquo;s an overview of your contacts. Manage or create new
-              ones with ease!
-            </p>
-          </div>
           <Button className="px-3">Add Contact</Button>
         </div>
         {/* Numbers */}
--- client/src/routes/admin/index.tsx
+++ client/src/routes/admin/index.tsx
@@ -72,7 +72,17 @@ function AdminHome() {
     <AdminLayout>
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+          <h2 className="text-3xl font-bold tracking-tight">
+            Admin Dashboard
+            <div className="space-y-1">
+              <h1 className="text-2xl pt-2 font-semibold">Oilà, Larry!</h1>
+              <p className="text-sm text-muted-foreground">
+                Here&rsquo;s an overview of your . Manage or create new ones
+                with ease!
+              </p>
+            </div>
+          </h2>
+
           <div className="flex items-center space-x-2">
             <Button>Download Report</Button>
           </div>
