57369b93fdb4b9e0156ec28fe843d903466a4f95
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-08-29 19:27:23 +0100

    add layouts

--- client/src/components/layout/AdminLayout.tsx
+++ client/src/components/layout/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/components/integrations/authContext";
-// import ProtectedRoute from "~/components/shared/protectedRoute";
+import ProtectedRoute from "@/components/shared/protectedRoute";
 import { AppSidebar } from "@/components/admin/app-sidebar";
 import {
   Breadcrumb,
@@ -29,48 +29,48 @@ interface AdminLayoutProps {
 export default function AdminLayout({ children }: AdminLayoutProps) {
   const { role } = useAuth(); // Get the user role
   return (
-    // <ProtectedRoute requiredRole="admin">
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-            <div className="flex flex-1 items-center gap-2 px-3">
-              <SidebarTrigger className="-ms-4" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">
-                      <CircleGauge size={22} aria-hidden="true" />
-                      <span className="sr-only">Admin</span>
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Contacts</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-            <div className="flex gap-3 ml-auto">
-              <FeedbackDialog />
-              <UserDropdown />
-            </div>
-          </header>
-          {children}
-          <Toaster />
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
-    // </ProtectedRoute>
+    <ProtectedRoute requiredRole="admin">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+              <div className="flex flex-1 items-center gap-2 px-3">
+                <SidebarTrigger className="-ms-4" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        <CircleGauge size={22} aria-hidden="true" />
+                        <span className="sr-only">Admin</span>
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Contacts</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+              <div className="flex gap-3 ml-auto">
+                <FeedbackDialog />
+                <UserDropdown />
+              </div>
+            </header>
+            {children}
+            <Toaster />
+          </SidebarInset>
+        </SidebarProvider>
+      </ThemeProvider>
+    </ProtectedRoute>
   );
 }
--- client/src/components/layout/DashboardLayout.tsx
+++ client/src/components/layout/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/components/integrations/authContext";
-// import ProtectedRoute from "~/components/shared/protectedRoute";
+import ProtectedRoute from "@/components/shared/protectedRoute";
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
 import {
   Breadcrumb,
@@ -30,51 +30,51 @@ interface DashboardLayoutProps {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const { role } = useAuth(); // Get the user role
   return (
-    // <ProtectedRoute requiredRole="admin">
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-            <div className="flex flex-1 items-center gap-2 px-3">
-              <SidebarTrigger className="-ms-4" />
-              <Separator
-                orientation="vertical"
-                className="mr-2 data-[orientation=vertical]:h-4"
-              />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">
-                      <CircleGauge size={22} aria-hidden="true" />
-                      <span className="sr-only">Dashboard</span>
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Contacts</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
-            <div className="flex gap-3 ml-auto">
-              <ModeToggle />
-              <FeedbackDialog />
-              <UserDropdown />
-            </div>
-          </header>
+    <ProtectedRoute requiredRole="admin">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
+            <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+              <div className="flex flex-1 items-center gap-2 px-3">
+                <SidebarTrigger className="-ms-4" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        <CircleGauge size={22} aria-hidden="true" />
+                        <span className="sr-only">Dashboard</span>
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Contacts</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+              <div className="flex gap-3 ml-auto">
+                <ModeToggle />
+                <FeedbackDialog />
+                <UserDropdown />
+              </div>
+            </header>
 
-          {children}
+            {children}
 
-          <Toaster />
-        </SidebarInset>
-      </SidebarProvider>{" "}
-    </ThemeProvider>
-    // </ProtectedRoute>
+            <Toaster />
+          </SidebarInset>
+        </SidebarProvider>{" "}
+      </ThemeProvider>
+    </ProtectedRoute>
   );
 }
