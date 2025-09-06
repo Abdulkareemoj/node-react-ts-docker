33950ef3692f49660546900650a3f9b18f3f1eff
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-09-06 07:51:54 +0100

    fixed model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -20,8 +20,10 @@ export interface UserDocument extends UserInput, mongoose.Document {
 const UserSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     role: {
       type: String,
       enum: ["user", "admin", "superadmin"],
@@ -55,9 +57,7 @@ UserSchema.methods.comparePassword = async function (
   candidatePassword: string
 ): Promise<boolean> {
   const user = this as UserDocument;
-  return bcrypt
-    .compare(candidatePassword, this.password)
-    .catch((e: any) => false);
+  return bcrypt.compare(candidatePassword, user.password);
 };
 
 const User = mongoose.model<UserDocument>("User", UserSchema);
