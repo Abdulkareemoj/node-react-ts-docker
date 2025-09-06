2cddc6fda5c4800861e8cfe223686d9aae9b5bb1
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-09-06 07:52:45 +0100

    update

--- server/src/schema/createShortUrl.schema.ts
+++ server/src/schema/createShortUrl.schema.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const createShortURLSchema = z.object({
   body: z.object({
     destination: z
-      .string()
       .url({ message: "Must be a valid URL" })
       .min(1, "Destination is required"),
   }),
