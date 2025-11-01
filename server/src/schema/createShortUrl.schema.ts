473f9ce0283ccbc199be9a91c4d6d26011cbbe29
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-11-01 23:55:03 +0100

    deleted

--- server/src/schema/createShortUrl.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const createShortURLSchema = z.object({
-  body: z.object({
-    destination: z
-      .url({ message: "Must be a valid URL" })
-      .min(1, "Destination is required"),
-  }),
-});
